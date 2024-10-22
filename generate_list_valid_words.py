da6b645766b9cb6391ac80c1b7d79ddf31be3869
MICHOUD KEVIN <kmichoud@unistra.fr>
2024-10-22 11:07:56 +0200

    after refacto

--- generate_list_valid_words.py
+++ generate_list_valid_words.py
@@ -40,5 +40,5 @@ def main(path):
         ecrire_liste_mots(lst_mots, longueur=long)
 
 if __name__ == '__main__':
-    chemin = "data/liste_francais.txt"
+    chemin = "data/liste_mots.txt"
     main(path = chemin)
