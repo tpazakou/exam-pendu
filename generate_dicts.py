4ef2b5bd1990d1d990ab97f698817bdf9afb0f58
THEODORA PAZAKOU <theodora.pazakou@gmail.com>
2024-11-02 20:36:49 +0100

    version 2. Function2 nt working

--- generate_dicts.py
+++ generate_dicts.py
@@ -12,36 +12,49 @@ On enlève les accents, les espaces, les tirets et les mots en double.
 """
 
 
-def lire_filtrer_mots(chemin_lexique, longueur):
-    """ Doc"""
+def lire_filtrer_mots(chemin_lexique: str, longueur: int) -> list[str]:
+    """
+    Lit le fichier de lexique trouvé dans le chemin indiqué et retourne une liste de mots d'une longueur spécifiée,
+    en majuscules, sans accents, apostrophes et tirets.
+
+    Args:
+        chemin_lexique (str): Chemin d'accès au fichier de lexique contenant les mots
+        longueur (int): Longueur des mots
+
+    Returns:
+        list: Une liste de mots filtrés, en majuscules et sans accents,
+              correspondant à la longueur spécifiée.
+
+    """
     with open(chemin_lexique, 'r', encoding='utf8') as f:
+        # set pour éviter les doublons
         liste_mots = set()
         for line in f.readlines():
+            # choisir le premier mot de la ligne
             mot = line.split(" ")[0]
             if len(mot) == longueur and "'" not in mot and "-" not in mot:
                 liste_mots.add(unidecode(mot.upper()))
     return list(liste_mots)
 
 
-def ecrire_liste_mots(liste_mots:list, longueur:int) -> None:
+def ecrire_liste_mots(liste_mots: list, longueur: int) -> None:
     """Génère un fichier texte contenant tous les mots pour une longueur donné"""
 
-    chemin_dico_ecriture:str = f"data/dico_{longueur}_lettres.txt"
+    chemin_dico_ecriture: str = f"data/dico_{longueur}_lettres.txt"
 
     with open(chemin_dico_ecriture, 'w', encoding='utf-8') as file:
         file.writelines(f"{mot}\n" for mot in liste_mots)
 
 
-
-
-def main(chemin:str) -> None:
-    for long in range(6,11):
+def main(chemin: str) -> None:
+    for long in range(6, 11):
         # génère la liste de mot pour la longueur donné
         lst_mots = lire_filtrer_mots(chemin_lexique=chemin, longueur=long)
 
         # Génère un fichier texte correspondant
         ecrire_liste_mots(lst_mots, longueur=long)
 
+
 if __name__ == '__main__':
     chemin = "data/liste_mots.txt"
-    main(chemin= chemin)
+    main(chemin=chemin)
--- solver.py
+++ solver.py
@@ -2,16 +2,75 @@ import math
 from copy import copy
 
 
-def generate_valid_words(possible_words, letters_in_secret, letters_not_in_secret):
-    """ Doc """
+def generate_valid_words(possible_words: list[str], letters_in_secret: list[tuple[str, int]], letters_not_in_secret: list[str]):
+    """
+    Génère une liste des mots valides.
+    Mots valides : Mots qui ne contiennent pas des lettres exclues et ayant les lettres déjà dévinées
+    à la bonne position.
+
+    Args:
+        possible_words (List[str]): Une liste de tous les mots potentiellement valides.
+
+        letters_in_secret (List[Tuple[str, int]]): Une liste de tuples représentant les lettres déjà trouvées par
+        l'utilisateur ainsi que leur position dans le mot.
+
+        letters_not_in_secret (List[str]): Une liste des lettres déjà essayées par l'utilisateur mais qui ne sont
+         pas dans le mot.
+
+    Returns:
+        List[str]: Une liste de mots valides qui répondent aux critères spécifiés.
+    """
+
     valid_words = []
+
+    for word in possible_words:
+        valid = True
+        for letter in word:
+            if letter in letters_not_in_secret:
+                valid = False
+                break
+        for letter, pos in letters_in_secret:
+            if word[pos] != letter:
+                valid = False
+                break
+        if valid:
+            valid_words.append(word)
     return valid_words
 
 
 
 
-def generate_best_letters(possible_words:list, letters_not_played:list[str], letters_in_secret, letters_not_in_secret):
-    return "Ajoutez votre implémentation ici"
+def generate_best_letters(possible_words:list, letters_not_played:list[str]):
+
+
+    letter_average = []
+
+    for letter in letters_not_played:
+        total_possibilities = 0
+        letters_in_secret = []
+        letters_not_in_secret = []
+
+        for word in possible_words:
+            secret_word = word
+            if letter in secret_word:
+                letter_pos_tuple = (letter, secret_word.index(letter))
+                letters_in_secret.append(letter_pos_tuple)
+            else:
+                letters_not_in_secret.append(letter)
+
+            nb_of_valid_words = len(generate_valid_words(possible_words, letters_in_secret, letters_not_in_secret))
+            total_possibilities += nb_of_valid_words
+        average = total_possibilities/len(possible_words)
+        letter_average.append(average)
+
+    minimum = min(letter_average)
+    pos = letter_average.index(minimum)
+    suggested_letter = letters_not_played[pos]
+    return f"La meilleure lettre à jouer est {suggested_letter}"
+
+
+
+
 
 
 
